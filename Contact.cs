68b54c9a24aab5379b2ddb865117432041409a5e
Rose Wisotzky <rose@rosewisotzky.com>
2019-06-03 14:20:35 -0500

    completed exercise with comments

---
+++ Contact.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace address_book_practice {
+    public class Contact {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string FullName {
+            get {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
